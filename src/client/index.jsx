31375c8c8642893c4e573a17d7623f61bc2f9b23
tansiewling-hotmail <prudence_tan@hotmail.com>
2019-11-20 16:23:19 +0800

    Display tweet text

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import tweets from 'tweets.js';
 
 class App extends React.Component {
   render() {
+
+    let displayTweet = this.props.tweetsIn.map((singleTweet) => {
+        return(
+            <li>{singleTweet.text}</li>
+        );
+    });
     return (
       <div>
-        tweets
+        {displayTweet}
       </div>
     );
   }
@@ -13,6 +20,8 @@ class App extends React.Component {
 
 const element = document.getElementById('app');
 
-ReactDOM.render(<App />, element );//
+ReactDOM.render(<App tweetsIn = {tweets} />, element );
 
 console.log("tweet react");
+console.log(tweets);
+console.log(tweets[0]);
