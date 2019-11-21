81108e27ec17a20789c5b9c3557e58c00f83f0dc
tansiewling-hotmail <prudence_tan@hotmail.com>
2019-11-21 09:27:41 +0800

    Changed to use nested User jsx for display of name and screen name

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -1,17 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import User from 'user.jsx';
 import tweets from 'tweets.js';
 
 class App extends React.Component {
   render() {
 
-    let displayTweet = this.props.tweetsIn.map((singleTweet) => {
+     let displayTweet = this.props.tweetsIn.map((singleTweet) => {
         return(
-            <li>{singleTweet.text}</li>
+            <div class="row">
+                <div class="col">
+                    <img src={singleTweet.user.profile_image_url} />
+                </div>
+                <div class="col">
+                    {/*<p>{singleTweet.user.name} @{singleTweet.user.screen_name}</p>*/}
+                    <User userInfo={singleTweet.user} />
+                    <p>{singleTweet.text}</p>
+                </div>
+            </div>
         );
     });
+
     return (
-      <div>
+      <div id="container">
         {displayTweet}
       </div>
     );
@@ -25,3 +36,4 @@ ReactDOM.render(<App tweetsIn = {tweets} />, element );
 console.log("tweet react");
 console.log(tweets);
 console.log(tweets[0]);
+console.log(tweets[0].entities.media[0].media_url);
